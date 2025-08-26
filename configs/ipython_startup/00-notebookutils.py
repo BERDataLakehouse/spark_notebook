b203d14253e77939527d90be998833b449a153cf
bio-boris <bio-boris@github.com>
2025-08-25 22:20:48 -0500

    Update fields

--- configs/ipython_startup/00-notebookutils.py
+++ configs/ipython_startup/00-notebookutils.py
@@ -2,3 +2,4 @@ from berdl_notebook_utils.berdl_settings import BERDLSettings, get_settings  # n
 from berdl_notebook_utils.clients import get_minio_client, get_task_service_client  # noqa F401
 from berdl_notebook_utils.setup_spark_session import get_spark_session  # noqa F401
 from berdl_notebook_utils.spark_utils import display_df, spark_to_pandas, read_csv  # noqa F401
+from berdl_notebook_utils import berdl_notebook_help # noqa F401
