288ef96fd06911acb61e89ee182d60c68400e750
Tianhao-Gu <tgu@anl.gov>
2025-09-22 15:49:48 -0500

    turn log back on

--- notebook_utils/berdl_notebook_utils/setup_spark_session.py
+++ notebook_utils/berdl_notebook_utils/setup_spark_session.py
@@ -182,7 +182,7 @@ def _get_s3_conf(settings: BERDLSettings, tenant_name: str | None = None) -> dic
         "spark.hadoop.fs.s3a.path.style.access": "true",
         "spark.hadoop.fs.s3a.impl": "org.apache.hadoop.fs.s3a.S3AFileSystem",
         "spark.sql.warehouse.dir": warehouse_dir,
-        "spark.eventLog.enabled": "false",  # TODO: Enable event logging
+        "spark.eventLog.enabled": "true",
         "spark.eventLog.dir": event_log_dir,
         "spark.sql.extensions": "io.delta.sql.DeltaSparkSessionExtension",
         "spark.sql.catalog.spark_catalog": "org.apache.spark.sql.delta.catalog.DeltaCatalog",
