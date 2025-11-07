6cfb8849345457f8b6f8acdf4e718c49523084f3
ialarmedalien <ialarmedalien@gmail.com>
2025-11-07 08:53:48 -0800

    Updating GHA versions and adding tests to execution

---
+++ notebook_utils/run_tests.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -x
+
+# Get the directory where the script is located
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+
+# Change the current working directory to the parent of SCRIPT_DIR
+cd "$SCRIPT_DIR"
+
+uv run pytest
